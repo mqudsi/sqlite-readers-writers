ad05b5b890315f01a7ec0248da9febc65fc54bdf
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2022-10-20 15:12:16 -0500

    Summarize results in a table at end of output

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::sync::{mpsc, Arc, RwLock};
 use std::thread;
 use std::time::{Duration, Instant};
 
+const ITER_SECS: u64 = 5;
 const USE_RWLOCK: bool = false;
 const SEED_COUNT: usize = 20;
 const NEW_ITEM_SIZE: usize = 40 * 1024;
@@ -119,7 +120,7 @@ fn read_loop(
     keys: &[u16],
     stop: Arc<AtomicBool>,
     rwlock: Arc<RwLock<()>>,
-) -> Vec<i64> {
+) -> (i32, Vec<i64>) {
     let mut times = Vec::new();
 
     let mut query = db
@@ -132,6 +133,7 @@ LIMIT 1;"#,
         )
         .expect("Failed to prepare query statement");
 
+    let mut reads = 0;
     let mut rng = FastRng::new();
     while !stop.load(Ordering::Relaxed) {
         let key_index = rng.get_usize() % keys.len();
@@ -143,6 +145,7 @@ LIMIT 1;"#,
             _guard = rwlock.read().expect("Cannot unlock for read!");
         }
         let value: Result<String, _> = query.query_row(&[key], |result| result.get(0));
+        reads += 1;
         let elapsed = timer.elapsed();
         if PRINT_VALUES {
             if let Ok(value) = value {
@@ -152,7 +155,7 @@ LIMIT 1;"#,
         times.push(elapsed.as_nanos() as i64);
     }
 
-    times
+    (reads, times)
 }
 
 fn write_loop(db: Database, stop: Arc<AtomicBool>, rwlock: Arc<RwLock<()>>) -> Vec<i64> {
@@ -199,8 +202,22 @@ fn average(nums: &[i64]) -> f64 {
     sum as f64 / (nums.len() as f64)
 }
 
-fn main() {
+struct PerfRecord {
+    config: String,
+    readers: i32,
+    writers: i32,
+    reads_per_sec: f64,
+    writes_per_sec: f64,
+    read_p95: f64,
+    read_p99: f64,
+    read_p999: f64,
+    write_p95: Option<f64>,
+    write_p99: Option<f64>,
+    write_p999: Option<f64>,
+}
 
+fn main() {
+    let mut perf_vec = Vec::new();
     for options in [
         DbOptions { shared_cache: false, wal: false },
         DbOptions { shared_cache: false, wal: true },
@@ -224,7 +241,7 @@ fn main() {
             {
                 let done = done.clone();
                 thread::spawn(move || {
-                    thread::sleep(Duration::from_secs(5));
+                    thread::sleep(Duration::from_secs(ITER_SECS));
                     done.store(true, Ordering::Release);
                 });
             }
@@ -240,22 +257,25 @@ fn main() {
                     let write_db = Database::open("test.db", &options);
                     let write_times = write_loop(write_db, done, rwlock);
                     sender
-                        .send(write_times.len())
+                        .send(write_times)
                         .expect("Could not send write count!");
                 });
             }
             drop(write_counts_send);
 
-            let mut read_times = read_loop(db, &keys, done.clone(), rwlock.clone());
+            let (total_reads, mut read_times) = read_loop(db, &keys, done.clone(), rwlock.clone());
             read_times.sort();
 
             let mut total_writes = 0;
+            let mut write_times = Vec::new();
             for _ in 0..writers {
-                let writes = write_counts_recv
+                let mut writes = write_counts_recv
                     .recv()
                     .expect("Failed to receive write counts!");
-                total_writes += writes;
+                total_writes += writes.len();
+                write_times.append(&mut writes);
             }
+            write_times.sort();
 
             println!("{} writers:", writers);
             println!("- Read {} values from the database.", read_times.len());
@@ -265,12 +285,54 @@ fn main() {
                 average(&read_times) / 1000_000f64
             );
             let p95_nanos = read_times[(0.95 * (read_times.len() as f64)) as usize];
-            println!("- P95: {} ms", p95_nanos as f64 / 1000_000f64);
+            let p95_millis = p95_nanos as f64 / 1000_000f64;
+            println!("- P95: {} ms", p95_millis);
             let p99_nanos = read_times[(0.99 * (read_times.len() as f64)) as usize];
-            println!("- P99: {} ms", p99_nanos as f64 / 1000_000f64);
+            let p99_millis = p99_nanos as f64 / 1000_000f64;
+            println!("- P99: {} ms", p99_millis);
             let p99_9_nanos = read_times[(0.999 * (read_times.len() as f64)) as usize];
-            println!("- P99.9: {} ms", p99_9_nanos as f64 / 1000_000f64);
+            let p99_9_millis = p99_9_nanos as f64 / 1000_000f64;
+            println!("- P99.9: {} ms", p99_9_millis);
             println!("");
+
+            fn not_str(v: bool) -> &'static str {
+                if v { "" } else { "!" }
+            }
+
+            perf_vec.push(PerfRecord {
+                config: format!("{}wal, {}shared_cache", not_str(options.wal), not_str(options.shared_cache)),
+                readers: 1,
+                writers,
+                reads_per_sec: total_reads as f64 / ITER_SECS as f64,
+                writes_per_sec: total_writes as f64 / ITER_SECS as f64,
+                read_p95: p95_millis,
+                read_p99: p99_millis,
+                read_p999: p99_9_millis,
+                write_p95: if write_times.len() > 0 { Some(write_times[(0.95 * (write_times.len() as f64)) as usize] as f64 / 1000_000f64) } else { None },
+                write_p99: if write_times.len() > 0 { Some(write_times[(0.99 * (write_times.len() as f64)) as usize] as f64 / 1000_000f64) } else { None },
+                write_p999: if write_times.len() > 0 { Some(write_times[(0.999 * (write_times.len() as f64)) as usize] as f64 / 1000_000f64) } else { None },
+            });
         }
     }
+
+    fn print_or<T: std::fmt::Display>(v: Option<T>, o: &str) -> String {
+        v.map(|v| v.to_string())
+            .unwrap_or(o.to_owned())
+    }
+
+    let title_width = perf_vec.iter().map(|r| r.config.len()).max().unwrap();
+
+    println!("---------------------------------");
+    println!("");
+    println!("| configuration | readers | writers | reads/sec | writes/sec | read p95 (ms) | read p99 | read p99.9 | write p95 | write p99 | write p99.9 |");
+    println!("|------------------------------------------------------------------------------------------------------------------------------------------|");
+    for row in perf_vec {
+        println!("| {:w0$} | {:2} | {:2} | {:>8.1} | {:>3.1} | {:>3.4} | {:>3.4} | {:>3.4} | {:>3.4} | {:>3.4} | {:>4.4} |",
+            row.config, row.readers, row.writers, row.reads_per_sec, row.writes_per_sec,
+            row.read_p95, row.read_p99, row.read_p999,
+            print_or(row.write_p95, "N/A"), print_or(row.write_p99, "N/A"), print_or(row.write_p999, "N/A"),
+            w0 = title_width,
+        );
+    }
+    println!("|------------------------------------------------------------------------------------------------------------------------------------------|");
 }
